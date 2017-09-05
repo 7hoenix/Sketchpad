42ee20f53f11c91b16aa83fb947280a8365f045d
7hoenix <jpholzmann@gmail.com>
2017-09-05 12:02:44 -0700

    Add channel tests

---
+++ test/sketchpad_web/channels/pad_channel_test.exs
@@ -0,0 +1,24 @@
+defmodule SketchpadWeb.PadChannelTest do
+  use SketchpadWeb.ChannelCase
+  alias SketchpadWeb.PadChannel
+
+  setup do
+    socket = Phoenix.Socket.assign(socket(), :user_id, "7hoenix")
+    assert {:ok, %{msg: "hi"}, socket} =
+      subscribe_and_join(socket, PadChannel, "pad:lobby", %{})
+    {:ok, socket: socket}
+  end
+
+  test "clear event is broadcast to everyone but self", %{socket: socket} do
+    ref = push socket, "clear", %{}
+    assert_reply ref, :ok
+
+    assert_broadcast "clear", %{}
+  end
+
+  test "messages include publishing user", %{socket: socket} do
+    ref = push socket, "new_message", %{body: "hello"}
+    assert_reply ref, :ok
+    assert_broadcast "new_message", %{body: "hello", user_id: "7hoenix"}
+  end
+end
