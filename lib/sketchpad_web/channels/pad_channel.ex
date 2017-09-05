56c48834242e39a80058c03d9161e030ee5388f2
7hoenix <jpholzmann@gmail.com>
2017-09-05 11:15:03 -0700

    Add buttons and chat

--- lib/sketchpad_web/channels/pad_channel.ex
+++ lib/sketchpad_web/channels/pad_channel.ex
@@ -1,9 +1,34 @@
 defmodule SketchpadWeb.PadChannel do
   use SketchpadWeb, :channel
 
+  # intercept "stroke" SUPER EXPENSIVE
+
   # this is process specific for each browser
   def join("pad:" <> pad_id, _params, socket) do
     IO.puts ">> #{socket.assigns.user_id} joined pad #{pad_id}"
     {:ok, %{msg: "hi"}, assign(socket, :pad_id, pad_id)}
+  end
+
+  def handle_in("stroke", stroke, socket) do
+    broadcast_from!(socket, "stroke", %{
+      stroke: stroke,
+      user_id: socket.assigns.user_id
+    })
+
+    {:reply, :ok, socket}
+  end
+
+  def handle_in("clear", _, socket) do
+    broadcast_from!(socket, "clear", %{user_id: socket.assigns.user_id})
+    {:reply, :ok, socket}
+  end
+
+  def handle_in("new_message", %{"body" => body}, socket) do
+    broadcast!(socket, "new_message", %{
+      user_id: socket.assigns.user_id,
+      body: body
+    })
+
+    {:reply, :ok, socket}
   end
 end
