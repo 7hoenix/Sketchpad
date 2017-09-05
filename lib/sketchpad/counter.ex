e3de00f58f9f7bf65b6f86dd94970bcae5d2459c
7hoenix <jpholzmann@gmail.com>
2017-09-05 11:58:43 -0700

    Switch counter to GenServer

--- lib/sketchpad/counter.ex
+++ lib/sketchpad/counter.ex
@@ -1,32 +1,32 @@
 defmodule Counter do
 
-  def inc(counter), do: send(counter, :inc)
-  def dec(counter), do: send(counter, :dec)
+  # FOR TESTING
+  # Process.register(self(), :foo)
+  # Process.whereis(Counter)
+  def inc(counter \\ __MODULE__), do: GenServer.cast(counter, :inc)
+  def dec(counter \\ __MODULE__), do: GenServer.cast(counter, :dec)
 
   def val(counter, timeout \\ 5000) do
-    ref = make_ref()
-    send(counter, {:val, ref, self()})
-    receive do
-      {:val, ^ref, val} -> val
-    after timeout -> exit(:timeout)
-    end
+    GenServer.call(counter, :val, timeout)
   end
 
   def start_link(initial_value \\ 0) do
-    {:ok,
-     spawn_link(fn ->
-       Process.register(self(), __MODULE__)
-       listen(initial_value)
-     end)}
+    GenServer.start_link(__MODULE__, [initial_value], name: __MODULE__)
   end
 
-  defp listen(val) do
-    receive do
-      :inc -> listen(val + 1)
-      :dec -> listen(val - 1)
-      {:val, ref, sender} ->
-        send sender, {:val, ref, val}
-        listen(val)
-    end
+  def init([initial_value]) do
+    {:ok, initial_value}
+  end
+
+  def handle_cast(:inc, val) do
+    {:noreply, val + 1}
+  end
+
+  def handle_cast(:dec, val) do
+    {:noreply, val - 1}
+  end
+
+  def handle_call(:val, _from, val) do
+    {:reply, val, val}
   end
 end
