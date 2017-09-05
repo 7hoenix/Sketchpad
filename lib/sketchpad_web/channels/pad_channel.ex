ae2504b014c88722217bb910d99fd7c0bf873f89
7hoenix <jpholzmann@gmail.com>
2017-09-05 14:27:13 -0700

    Add Pad GenServer to store state across refresh

--- lib/sketchpad_web/channels/pad_channel.ex
+++ lib/sketchpad_web/channels/pad_channel.ex
@@ -1,25 +1,57 @@
 defmodule SketchpadWeb.PadChannel do
   use SketchpadWeb, :channel
 
+  alias Sketchpad.Pad
   # intercept "stroke" SUPER EXPENSIVE
+
+  def broadcast_clear(topic, user_id) do
+    SketchpadWeb.Endpoint.broadcast!(topic, "clear", %{
+                                     user_id: user_id
+                                   })
+  end
 
   # this is process specific for each browser
   def join("pad:" <> pad_id, _params, socket) do
-    IO.puts ">> #{socket.assigns.user_id} joined pad #{pad_id}"
-    {:ok, %{msg: "hi"}, assign(socket, :pad_id, pad_id)}
+    {:ok, pid} = Pad.find(pad_id)
+
+    send(self(), :after_join)
+
+    socket =
+      socket
+      |> assign(:pad_id, pad_id)
+      |> assign(:pad, pid)
+
+    {:ok, %{msg: "hi"}, socket}
+  end
+
+  def handle_info(:after_join, socket) do
+    pad = socket.assigns.pad
+
+    for {user_id, %{strokes: strokes}} <- Pad.render(pad) do
+      for stroke <- Enum.reverse(strokes) do
+        push(socket, "stroke", %{user_id: user_id, stroke: stroke})
+      end
+    end
+
+    {:noreply, socket}
+  end
+
+  def broadcast_stroke(topic, user_id, stroke) do
+    SketchpadWeb.Endpoint.broadcast!(topic, "stroke", %{
+                                     stroke: stroke,
+                                     user_id: user_id
+                                   })
   end
 
   def handle_in("stroke", stroke, socket) do
-    broadcast_from!(socket, "stroke", %{
-      stroke: stroke,
-      user_id: socket.assigns.user_id
-    })
-
+    Pad.put_stroke(socket.assigns.pad,
+                   socket.assigns.user_id,
+                   stroke)
     {:reply, :ok, socket}
   end
 
   def handle_in("clear", _, socket) do
-    broadcast_from!(socket, "clear", %{user_id: socket.assigns.user_id})
+    Pad.clear(socket.assigns.pad, socket.assigns.user_id)
     {:reply, :ok, socket}
   end
 
